2222201156265d8300050ef5d5297c5b780f22c5
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-01-09 20:58:12 +0500

    styled btns

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -9,32 +9,34 @@ import {
 
 export const ActionCancelBtn = styled(Button)<ButtonProps>(() => ({
   color: mainColor,
-  border: `1px solid ${mainColor}`,
+  border: `2px solid ${mainColor}`,
   borderRadius: '20px',
+  boxSizing: 'border-box',
 
   '&:hover': {
     color: subtitleColor,
-    borderColor: subtitleColor,
+    border: `2px solid ${subtitleColor}`,
   },
 
   '&.Mui-disabled': {
     color: 'rgba(255, 255, 255, 0.3)',
-    borderColor: 'rgba(255, 255, 255, 0.3)',
+    border: `2px solid rgba(255, 255, 255, 0.3)`,
   },
 }));
 
 export const ActionConfirmBtn = styled(Button)<ButtonProps>(() => ({
   color: successColor,
-  border: `1px solid ${successColor}`,
+  border: `2px solid ${successColor}`,
   borderRadius: '20px',
+  boxSizing: 'border-box',
 
   '&:hover': {
     color: 'rgba(16, 194, 0, 0.6)',
-    borderColor: 'rgba(16, 194, 0, 0.6)',
+    border: `2px solid rgba(16, 194, 0, 0.6)`,
   },
 
   '&.Mui-disabled': {
     color: 'rgba(16, 194, 0, 0.3)',
-    borderColor: 'rgba(16, 194, 0, 0.3)',
+    border: `2px solid rgba(16, 194, 0, 0.3)`,
   },
 }));
