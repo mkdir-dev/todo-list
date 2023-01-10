f990ef87bdb84396803bbe48ee39cfe1137ab3ab
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-01-10 18:41:31 +0500

    fix regExp

--- src/utils/helpers/dateHelpers.ts
+++ src/utils/helpers/dateHelpers.ts
@@ -2,7 +2,6 @@ import { format } from 'date-fns';
 
 export const regExpDate = /^[0-9]{2}[.][0-9]{2}[.][0-9]{4}$/g;
 export const regExpDateForSafari = /[0-9]{2}\/[0-9]{1,2}\/[0-9]{4}/m;
-//
 
 export function datePrevalidator(newValue: Date | null): string {
   return newValue &&
