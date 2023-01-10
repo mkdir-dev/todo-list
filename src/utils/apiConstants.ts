a00032148d002343a499e3ebd872e57873b1b696
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-01-10 19:20:32 +0500

    fix deploy

--- src/utils/apiConstants.ts
+++ src/utils/apiConstants.ts
@@ -1,2 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL;
-export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_URL =
+  process.env.REACT_APP_API_URL ?? 'https://newsapi.org/v2';
+export const API_KEY =
+  process.env.REACT_APP_API_KEY ?? '9efc2540a4ea4446bb6737129641046e';
